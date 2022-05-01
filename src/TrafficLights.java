b61be2bf5fb154bcc0a8a1fb25f97ea7b84ea509
Vladislav Kostin <92290777+yowawowa@users.noreply.github.com>
2022-05-01 17:13:59 +0500

    traffic lights

--- src/TrafficLights.java
+++ src/TrafficLights.java
@@ -1,8 +1,14 @@
 public class TrafficLights {
 
     public static String updateLight(String current) {
-
-        return "";
+        switch (current) {
+            case "green":
+                return "yellow";
+            case "yellow":
+                return "red";
+            case "red":
+                return "green";
+            default: return "not a light";
+        }
     }
-
 }
